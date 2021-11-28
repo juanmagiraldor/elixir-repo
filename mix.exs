77af76b25e917eaa9e29f1469e36edef79e66765
juanmagiraldor <juan.giraldo@kommit.co>
2021-11-28 17:59:55 -0500

    add distillery

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Hello.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:edeliver, "~> 1.8.0"}
+      {:edeliver, "~> 1.8.0"},
+      {:distillery, "~> 2.1"}
     ]
   end
 
