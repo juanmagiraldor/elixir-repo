a84f9e3f9f38e60be42d103f8305c1146da80a47
juanmagiraldor <juan.giraldo@kommit.co>
2021-11-29 08:35:24 -0500

    update database name

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :database, Hello.Repo,
+config :hello, Hello.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
